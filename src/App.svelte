9f7acf1d4cbe62663216481e91fd9011615ce4f0
dasomko <ds92ko@gmail.com>
2024-12-20 13:05:26 +0900

    feat: 이벤트 기능으로 좋아요 버튼 만들기

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,26 @@
 <script>
-  import logo1 from './assets/svelte.svg';
-  let logo2 = '/vite.svg';
-  let textRed = "color: red;";
   const data = [
-    { title: '고질라 X 콩: 뉴 엠파이어', year: 2024, category: '어드벤처, 액션, SF' },
-    { title: '듄: 파트2', year: 2024, category: '액션' },
+    { title: '파묘', year: 2024, category: '미스터리, 공포', likeCount: 0},
+    { title: '고질라 X 콩: 뉴 엠파이어', year: 2024, category: '어드벤처, 액션, SF', likeCount: 0 },
+    { title: '듄: 파트2', year: 2024, category: '액션', likeCount: 0 },
   ]
+  
+  /**
+   * @param {number} index
+   */
+  const handleLike = (index) => {
+    data[index].likeCount += 1;
+  }
 </script>
 
 <main>
   <h1>영화정보</h1>
-  <img src={logo1} alt="Svelte logo" />
-  <img src={logo2} alt="Vite logo" />
-  {#each data as { title, year, category }}
+  {#each data as { title, year, category, likeCount }, index}
     <div>
-      <h3 class="bg-yellow" style={textRed}>{title}</h3>
+      <h3 class="bg-yellow">{title}</h3>
       <p>개봉: {year}</p>
       <p>장르: {category}</p>
+      <button on:click={() => handleLike(index)}>좋아요 {likeCount}</button>
     </div>
   {/each}
 </main>
@@ -25,5 +29,6 @@
   .bg-yellow {
     background: yellow;
     padding: 10px;
+    color: #333;
   }
 </style>
