9054ba5bd742fc1730a430ddf094d2d496a9015d
dasomko <ds92ko@gmail.com>
2024-12-20 22:27:14 +0900

    feat: 동적 class 다루기

--- src/App.svelte
+++ src/App.svelte
@@ -2,14 +2,15 @@
   import MoviesData from "./lib/movies";
   import Movies from "./lib/components/Movies.svelte";
   import Modal from "./lib/components/Modal.svelte";
+  import NavBar from "./lib/components/NavBar.svelte";
 
   let data = [...MoviesData];
+  let isOpenEvent = true;
   let isOpenModal = false;
   /**
    * @type {number | null}
    */
   let selectedMovie = null;
-
   /**
    * @param {number} index
    */
@@ -27,10 +28,49 @@
     isOpenModal = false;
     selectedMovie = null;
   } 
+  const handleCloseEvent = () => {
+    isOpenEvent = !isOpenEvent;
+  }
 </script>
 
+<NavBar />
+<div class={`event ${isOpenEvent ? 'show' : ''}`}>
+  <p>NETFLIX 강렬한 운명의 드라마, 경기크리처</p>
+  <button on:click={handleCloseEvent}>X</button>
+</div>
 <Movies {data} {handleLike} {handleOpenModal} />
-
 {#if isOpenModal}
   <Modal {data} {selectedMovie} {handleCloseModal} />
 {/if}
+
+<style>
+  .event {
+    width: 100%;
+    background: #666;
+    padding: 5px 1em;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: #fff;
+    text-align: center;
+    margin-bottom: 1em;
+
+    max-height: 0;
+    opacity: 0;
+    overflow: hidden;
+    transition: all .3s;
+
+    &.show {
+      max-height: 100px;
+      opacity: 1;
+    }
+
+    p {
+      margin-bottom: 0;
+    }
+
+    button {
+      padding: 2px;
+    }
+  }
+</style>
---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,38 @@
+<header class="header">
+  <nav>
+    <ul class="nav-list">
+      <li class="nav-item">
+        <a href="/" class="nav-link">Home</a>
+      </li>
+      <li class="nav-item">
+        <a href="/about" class="nav-link">About</a>
+      </li>
+      <li class="nav-item">
+        <a href="/contact" class="nav-link">Contact</a>
+      </li>
+    </ul>
+  </nav>
+</header>
+
+<style>
+  .header {
+    background: #333;
+    color: #fff;
+    padding: 1rem;
+  }
+
+  .nav-list {
+    display: flex;
+    justify-content: center;
+    list-style: none;
+
+    .nav-item {
+      margin-right: 1rem;
+
+      .nav-link {
+        color: #fff;
+        text-decoration: none;
+      }
+    }
+  }
+</style>
